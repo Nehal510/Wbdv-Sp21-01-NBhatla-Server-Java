a450982605b98074c3e47848cdfa6f79cf2aabe9
nehal <nehal@email.com>
2021-03-24 02:57:20 -0400

    A5

--- src/main/java/com/example/WbDvSp2101NbhatlaServerJava/DemoApplication.java
+++ src/main/java/com/example/WbDvSp2101NbhatlaServerJava/DemoApplication.java
@@ -3,11 +3,17 @@ package com.example.WbDvSp2101NbhatlaServerJava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port","8080"));
+		app.run(args);
+		//SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
